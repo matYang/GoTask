46f068751a8240344d4564f141c29d1f43725e11
matYang <uwse@me.com>
2014-07-11 10:38:32 +0800

    finishing the start tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"GoTask/newmath"
+	"fmt"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello World\n")
+	fmt.Printf("Hello, world, Sqrt(2) = %v\n", newmath.Sqrt(2))
+	susie()
+}
+
+func susie() {
+
+	fmt.Printf("susie\n")
 }
